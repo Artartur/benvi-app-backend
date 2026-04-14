2e2339a8abc1377a713cd30b6b7845bffcb42b69
artartur <arturgfs@hotmail.com>
2026-04-14 19:56:14 -0300

    refactor(patient): extract BaseController with shared CRUD methods

--- src/patient/controller/patient.controller.ts
+++ src/patient/controller/patient.controller.ts
@@ -9,67 +9,29 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { BaseController } from 'src/base/base.controller';
 import { PatientDocument } from 'src/schema/patient.schema';
 import { CreatePatientDto, UpdatePatientDto } from '../dto/patient.dto';
 import { PatientRepository } from '../repository/patient.repository';
 import { PatientService } from '../service/patient.service';
-import { formatMultipleTimezones, formatTimezone } from 'src/utils/formatData';
 
 @Controller('patient')
-export class PatientController {
+export class PatientController extends BaseController<
+  PatientDocument,
+  CreatePatientDto,
+  UpdatePatientDto
+> {
   constructor(
     private patientRepository: PatientRepository,
     private patientService: PatientService,
-  ) {}
-
-  @Get()
-  async findAll(
-    @Query('page') page = 1,
-    @Query('limit') limit = 10,
-    @Query('isActive') isActive = true,
   ) {
-    const patients = await this.patientRepository.findAllByStatus(
-      isActive,
-      Number(page),
-      Number(limit),
-    );
-
-    return formatMultipleTimezones(patients);
-  }
-
-  @Get('id/:id')
-  async findById(@Param('id') id: string) {
-    const patient = await this.patientService.findById(id);
-
-    return formatTimezone(patient);
+    super(patientService);
   }
 
   @Get('cpf/:cpf')
   async findByCpf(@Param('cpf') cpf: string) {
-    const patient = (await this.patientService.findByCpf(
-      cpf,
-    )) as PatientDocument;
+    const patient = await this.patientService.findByCpf(cpf);
 
-    return formatTimezone(patient);
-  }
-
-  @Post()
-  async create(@Body() patientData: CreatePatientDto) {
-    return this.patientService.create(patientData);
-  }
-
-  @Patch(':id/deactivate')
-  async deactivate(@Param('id') id: string) {
-    return this.patientService.deactivate(id);
-  }
-
-  @Put(':id')
-  async update(@Body() patientData: UpdatePatientDto, @Param('id') id: string) {
-    return this.patientService.update(id, patientData);
-  }
-
-  @Delete(':id')
-  async delete(@Param('id') id: string) {
-    return this.patientService.delete(id);
+    return patient;
   }
 }
