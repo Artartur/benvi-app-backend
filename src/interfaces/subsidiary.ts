cd048f1f96b79081a663213fe239d4b1362b315f
artartur <arturgfs@hotmail.com>
2026-04-21 12:16:30 -0300

    feat(interface): create subsidiary

---
+++ src/interfaces/subsidiary.ts
@@ -0,0 +1,22 @@
+import { IsOptional, IsString } from 'class-validator';
+import { CreateHospitalDto } from 'src/hospital/dto/hospital.dto';
+import { OmitType } from '@nestjs/mapped-types';
+
+export class Subsidiary extends OmitType(CreateHospitalDto, [
+  'subsidiary',
+  'cnpj',
+  'email',
+  'healthCares',
+] as const) {
+  @IsString({ message: 'Esse campo é do tipo texto' })
+  @IsOptional()
+  cnpj?: string;
+
+  @IsString({ message: 'Esse campo é do tipo texto' })
+  @IsOptional()
+  email?: string;
+
+  @IsString({ message: 'Esse campo é do tipo texto' })
+  @IsOptional()
+  healthCares?: string[];
+}
